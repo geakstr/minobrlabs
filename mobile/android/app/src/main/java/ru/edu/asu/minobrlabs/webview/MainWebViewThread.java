01cc549adb436bfe9a034787db918c7971c24852
geakstr <geakstr@me.com>
2015-11-05 16:43:46 +0300

    Only landscape mode. Example dynamic data passing to JS

--- mobile/android/app/src/main/java/ru/edu/asu/minobrlabs/webview/MainWebViewThread.java
+++ mobile/android/app/src/main/java/ru/edu/asu/minobrlabs/webview/MainWebViewThread.java
@@ -11,13 +11,21 @@ public class MainWebViewThread extends AbstractWebViewThread {
 
     @Override
     public void run() {
-        final Message msg = handler.obtainMessage();
-        final Bundle bundle = new Bundle();
+        for (int i = 0; i <= 100; i += 1) {
+            final Message msg = handler.obtainMessage();
+            final Bundle bundle = new Bundle();
 
-        // TODO: Get data from detectors and put it here
-        bundle.putString("cmd", String.format("javascript:setHumidity('%s');", 33.5));
+            // TODO: Get data from detectors and put it here
+            bundle.putString("cmd", String.format("javascript:setHumidity('%s');", i));
 
-        msg.setData(bundle);
-        handler.sendMessage(msg);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+
+            try {
+                sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
