21622c2a399bc3d6d97b7795d23b0588f12f330e
geakstr <geakstr@me.com>
2015-11-11 23:35:07 +0300

    Microphone chart. Some refactoring

--- desktop/osx/minobrlabs/web/js/main.js
+++ desktop/osx/minobrlabs/web/js/main.js
@@ -1,296 +1,174 @@
 var charts = {};
+var xyzAxises = ['x', 'y', 'z'];
 
-charts.airTemperature = c3.generate({
-  bindto: '.chart-air-temperature',
-  data: {
-    type: 'gauge',
-    columns: [['data', 30]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return (value > 0 ? '+' : '-') + value + ' °C';
-      },
+function composeAxisesChart(opts) {
+  var chart, signs, selector;
+
+  signs = ['positive', 'negative'];
+
+  chart = {};
+
+  opts.axises.forEach(function(axis) {
+    chart[axis] = {};
+    signs.forEach(function(sign) {      
+      chart[axis][sign] = {};
+
+      selector = '.chart-' + opts.name + ' .chart-axises-' + sign + ' .chart-' + axis + '-axis';
+      chart[axis][sign].bar = document.querySelector(selector);
+      chart[axis][sign].val = document.querySelector(selector + ' .chart-axis-val');
+    });
+  });
+
+  return chart;
+}
+
+function composeGaugeChart(opts) {
+  return c3.generate({
+    bindto: '.chart-' + opts.name,
+    data: {
+      type: 'gauge',
+      columns: [['data', 0]],   
     },
-    min: -70,
-    max: 70,
-    width: 25
-  },
-  color: {
-    pattern: ['#00FF00', '#FFFF00', '#FFA500', '#FF0000'],
-    threshold: {
-      values: [35, 42, 49, 56]
+    gauge: {
+      label: {
+        format: opts.formatFunction,
+      },
+      min: opts.min,
+      max: opts.max,
+      width: 15
+    },
+    color: {
+      pattern: opts.color.pattern,
+      threshold: {
+        values: opts.color.threshold
+      }
+    },
+    interaction: {
+      enabled: false
+    },
+    transition: {
+      duration: null
     }
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.humidity = c3.generate({
-  bindto: '.chart-humidity',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + ' %';
-      },
-    },
-    min: 0,
-    max: 100,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.atmoPressure = c3.generate({
-  bindto: '.chart-atmo-pressure',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + ' кПа';
-      },
-    },
-    min: 0,
-    max: 300,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.light = c3.generate({
-  bindto: '.chart-light',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + ' лк';
-      },
-    },
-    min: 0,
-    max: 10000,
-    width: 25
-  },
-  color: {
-    pattern: ['#FFC107']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.soluteTemperature = c3.generate({
-  bindto: '.chart-solute-temperature',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return (value > 0 ? '+' : '-') + value + ' °C';
-      },
-    },
-    min: -50,
-    max: 1500,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.voltage = c3.generate({
-  bindto: '.chart-voltage',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + '  В';
-      },
-    },
-    min: -30,
-    max: 30,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.amperage = c3.generate({
-  bindto: '.chart-amperage',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + ' А';
-      },
-    },
-    min: -1,
-    max: 1,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.ph = c3.generate({
-  bindto: '.chart-ph',
-  data: {
-    type: 'gauge',
-    columns: [['data', 91.4]],   
-  },
-  gauge: {
-    label: {
-      format: function(value, ratio) {
-        return value + ' pH';
-      },
-    },
-    min: 0,
-    max: 14,
-    width: 25
-  },
-  color: {
-    pattern: ['#0000FF']
-  },
-  interaction: {
-    enabled: false
-  },
-  transition: {
-    duration: null
-  }
-});
-
-charts.gyro = {
-  x : {
-    positive : {
-      bar : document.querySelector('.chart-gyro .chart-axises-positive .chart-x-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-positive .chart-x-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-gyro .chart-axises-negative .chart-x-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-negative .chart-x-axis .chart-axis-val')  
-    }
-  },
-  y : {
-    positive : {
-      bar : document.querySelector('.chart-gyro .chart-axises-positive .chart-y-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-positive .chart-y-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-gyro .chart-axises-negative .chart-y-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-negative .chart-y-axis .chart-axis-val')  
-    }
-  },
-  z : {
-    positive : {
-      bar : document.querySelector('.chart-gyro .chart-axises-positive .chart-z-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-positive .chart-z-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-gyro .chart-axises-negative .chart-z-axis'),
-      val : document.querySelector('.chart-gyro .chart-axises-negative .chart-z-axis .chart-axis-val')  
-    }
-  }
-};
-
-charts.accel = {
-  x : {
-    positive : {
-      bar : document.querySelector('.chart-accel .chart-axises-positive .chart-x-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-positive .chart-x-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-accel .chart-axises-negative .chart-x-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-negative .chart-x-axis .chart-axis-val')  
-    }
-  },
-  y : {
-    positive : {
-      bar : document.querySelector('.chart-accel .chart-axises-positive .chart-y-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-positive .chart-y-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-accel .chart-axises-negative .chart-y-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-negative .chart-y-axis .chart-axis-val')  
-    }
-  },
-  z : {
-    positive : {
-      bar : document.querySelector('.chart-accel .chart-axises-positive .chart-z-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-positive .chart-z-axis .chart-axis-val')  
-    },
-    negative : {
-      bar : document.querySelector('.chart-accel .chart-axises-negative .chart-z-axis'),
-      val : document.querySelector('.chart-accel .chart-axises-negative .chart-z-axis .chart-axis-val')  
-    }
-  }
-};
-
+  });
+}
 
 var labels = {
-  gyro : document.querySelector('.label-gyro'),
-  accel : document.querySelector('.label-accel'),
   microphone : document.querySelector('.label-microphone')
 }
+
+charts.gyro = composeAxisesChart({
+  name : 'gyro',
+  axises : xyzAxises
+});
+
+charts.accel = composeAxisesChart({
+  name : 'accel',
+  axises : xyzAxises
+});
+
+charts.airTemperature = composeGaugeChart({
+  name : 'air-temperature',
+  formatFunction: function(value, ratio) {
+    return (value == 0 ? '' : (value > 0 ? '+' : '-')) + value + ' °C';
+  },
+  min: -70,
+  max: 70,
+  color : {
+    pattern: ['#0000FF', '#00FF00', '#FFFF00', '#FFA500', '#FF0000'],
+    threshold: [1, 20, 30, 40, 50]
+  }
+});
+
+charts.humidity = composeGaugeChart({
+  name : 'humidity',
+  formatFunction: function(value, ratio) {
+    return value + ' %';
+  },
+  min: 0,
+  max: 100,
+  color : {
+    pattern: ['#0000FF'],
+    threshold: []
+  }
+});
+
+charts.atmoPressure = composeGaugeChart({
+  name : 'atmo-pressure',
+  formatFunction: function(value, ratio) {
+    return value + ' кПа';
+  },
+  min: 0,
+  max: 300,
+  color : {
+    pattern: ['#0000FF'],
+    threshold: []
+  }
+});
+
+charts.light = composeGaugeChart({
+  name : 'light',
+  formatFunction: function(value, ratio) {
+    return value + ' лк';
+  },
+  min: 0,
+  max: 1000,
+  color : {
+    pattern: ['#FFC107'],
+    threshold: []
+  }
+});
+
+charts.soluteTemperature = composeGaugeChart({
+  name : 'solute-temperature',
+  formatFunction: function(value, ratio) {
+    return (value == 0 ? '' : (value > 0 ? '+' : '-')) + value + ' °C';
+  },
+  min: -50,
+  max: 1500,
+  color : {
+    pattern: ['#0000FF', '#00FF00', '#FFFF00', '#FFA500', '#FF0000'],
+    threshold: [1, 300, 600, 900, 1200]
+  }
+});
+
+charts.voltage = composeGaugeChart({
+  name : 'voltage',
+  formatFunction: function(value, ratio) {
+    return value + ' В';
+  },
+  min: -30,
+  max: 30,
+  color : {
+    pattern: ['#0000FF'],
+    threshold: []
+  }
+});
+
+charts.amperage = composeGaugeChart({
+  name : 'amperage',
+  formatFunction: function(value, ratio) {
+    return value + ' А';
+  },
+  min: -1,
+  max: 1,
+  color : {
+    pattern: ['#0000FF'],
+    threshold: []
+  }
+});
+
+charts.ph = composeGaugeChart({
+  name : 'ph',
+  formatFunction: function(value, ratio) {
+    return value + ' pH';
+  },
+  min: 0,
+  max: 14,
+  color : {
+    pattern: ['#0000FF'],
+    threshold: []
+  }
+});
 
 function airTemperature(v) {
   charts.airTemperature.load({
@@ -311,11 +189,11 @@ function light(v) {
 }
 
 function microphone(v) {
-  labels.microphone.textContent = v[0] + ' db';
+  labels.microphone.textContent = v[0] + ' дб';
 }
 
 function gyro(vals) {
-  axisesChart('gyro', ['x', 'y', 'z'], vals, function(val) {
+  axisesChart(charts.gyro, xyzAxises, vals, function(val) {
     return Math.min(Math.abs(val * 100.0), 100.0);
   });
 }
@@ -325,12 +203,12 @@ function accel(vals) {
     return val / 9.8;
   });
 
-  axisesChart('accel', ['x', 'y', 'z'], vals, function(val) {
+  axisesChart(charts.accel, xyzAxises, vals, function(val) {
     return Math.min(Math.abs(val * 100.0 / 2.0), 100.0);
   });
 }
 
-function axisesChart(name, axises, vals, widthFunction) {
+function axisesChart(chart, axises, vals, widthFunction) {
   var eps, axis, val, sign, oppositeSign, l, i;
 
   eps = 0.005;
@@ -349,12 +227,12 @@ function axisesChart(name, axises, vals, widthFunction) {
     sign = val >= 0.0 ? 'positive' : 'negative';
     oppositeSign = val >= 0.0 ? 'negative' : 'positive';
 
-    charts[name][axis][sign].val.style.display = 'inline';
-    charts[name][axis][sign].val.textContent = val.toFixed(2);  
-    charts[name][axis][oppositeSign].val.style.display = 'none';
+    chart[axis][sign].val.style.display = 'inline';
+    chart[axis][sign].val.textContent = val.toFixed(2);  
+    chart[axis][oppositeSign].val.style.display = 'none';
 
-    charts[name][axis][sign].bar.style.width = widthFunction(val) + '%';
-    charts[name][axis][oppositeSign].bar.style.width = 0;
+    chart[axis][sign].bar.style.width = widthFunction(val) + '%';
+    chart[axis][oppositeSign].bar.style.width = 0;
   }
 }
 
