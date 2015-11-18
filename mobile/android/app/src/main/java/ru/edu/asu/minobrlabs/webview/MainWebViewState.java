0ac81e92e8d39ebe8fabcce6411427a31b6c7dd5
geakstr <geakstr@me.com>
2015-11-18 19:50:34 +0300

    A little refactoring

--- mobile/android/app/src/main/java/ru/edu/asu/minobrlabs/webview/MainWebViewState.java
+++ mobile/android/app/src/main/java/ru/edu/asu/minobrlabs/webview/MainWebViewState.java
@@ -3,20 +3,22 @@ package ru.edu.asu.minobrlabs.webview;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainWebViewState {
-    public String os = "android";
+import ru.edu.asu.minobrlabs.sensors.SensorTypes;
 
-    public Map<String, Integer> charts = new HashMap<String, Integer>() {{
-        put("microphone", 1);
-        put("accel", 1);
-        put("gyro", 1);
-        put("airTemperature", 1);
-        put("humidity", 1);
-        put("atmoPressure", 1);
-        put("light", 1);
-        put("soluteTemperature", 1);
-        put("voltage", 1);
-        put("amperage", 1);
-        put("ph", 1);
+public class MainWebViewState {
+    public final String os = "android";
+
+    public final Map<String, Integer> charts = new HashMap<String, Integer>() {{
+        put(SensorTypes.MICROPHONE_DB, 1);
+        put(SensorTypes.ACCEL, 1);
+        put(SensorTypes.GYRO, 1);
+        put(SensorTypes.AIR_TEMPERATURE, 1);
+        put(SensorTypes.HUMIDITY, 1);
+        put(SensorTypes.AIR_PRESSURE, 1);
+        put(SensorTypes.LIGHT, 1);
+        put(SensorTypes.SOLUTE_TEMPERATURE, 1);
+        put(SensorTypes.VOLTAGE, 1);
+        put(SensorTypes.AMPERAGE, 1);
+        put(SensorTypes.PH, 1);
     }};
 }
